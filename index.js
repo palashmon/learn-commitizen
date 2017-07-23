32486edab68957788f29759dd37a9f2ade24ee55
Palash Mondal <palashm@live.in>
2017-07-23 11:44:28 +0530

    chore(releasing): Checking multiple commits in one release

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
-    sayHelloInEnglish: function() {
-        return "Hello";
+  sayHelloInEnglish() {
+      return "Hello";
     },
 
-    sayHelloInSpanish: function() {
-        return "Hola";
-    }
+  sayHelloInSpanish() {
+    return "Hola";
+  }
 };
